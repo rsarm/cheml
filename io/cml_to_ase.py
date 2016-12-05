c77824c4106ab8f2dc8c24c01230017e63cd06d3
rsarm <rsarmientoperez@gmail.com>
2016-12-05 15:46:44 +0100

    Add doc to to_ase

--- io/cml_to_ase.py
+++ io/cml_to_ase.py
@@ -5,7 +5,15 @@ from ase.atoms import Atoms
 
 
 def to_ase(ds,nmol,sublist):
-    """Returns a list of nmol ase.atoms.Atoms objects."""
+    """Returns a list of nmol ase.atoms.Atoms objects.
+
+    * ds      :: dataset objects
+    * nmol    :: number of molecules
+    * sublist :: list of indices of molecules to be converted to
+                 ase Atoms object.
+
+    """
+
     list_of_mol=[]
 
     if nmol==None:
