656bf26f1cc69491918b5789c0fd0ad3d2952ec3
rsarm <rsarmientoperez@gmail.com>
2017-06-02 11:30:36 +0200

    New desgin

--- ml/krr.py
+++ ml/krr.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from cheml.ml import kernels
 from cheml.ml.optimization import grid_search, simplex
+from cheml.tools.metrics  import euclidean2, manhattan
 
 
 
@@ -32,6 +33,58 @@ class krr(object):
                   # or NotImplementedError
 
 
+        if self.kernel=='rbf' or self.kernel=='gaussian':
+            self._kernel = kernels.rbf_kernel#(xi,xj,self.gamma)
+            self.kparams = (self.gamma,)
+            self._dismat = euclidean2
+
+        if self.kernel=='laplacian':
+            self._kernel = kernels.laplacian_kernel#(xi,xj,self.gamma)
+            self.kparams = (self.gamma,)
+            self._dismat = manhattan
+
+        if self.kernel=='multiquadric_euclidean' or self.kernel=='mql2':
+            self._kernel = kernels.multiquadric_euclidean_kernel#(xi,xj,self.c)
+            self.kparams = (self.c,)
+            self._dismat = euclidean2
+
+        if self.kernel=='multiquadric_cityblock' or self.kernel=='mql1':
+            self._kernel = kernels.multiquadric_cityblock_kernel#(xi,xj,self.c)
+            self.kparams = (self.c,)
+            self._dismat = manhattan
+
+        if self.kernel=='inv_multiquadric_euclidean' or self.kernel=='imql2':
+            self._kernel = kernels.inv_multiquadric_euclidean_kernel#(xi,xj,self.c)
+            self.kparams = (self.c,)
+            self._dismat = euclidean2
+
+        if self.kernel=='inv_multiquadric_cityblock' or self.kernel=='imql1':
+            self._kernel = kernels.inv_multiquadric_cityblock_kernel#(xi,xj,self.c)
+            self.kparams = (self.c,)
+            self._dismat = manhattan
+
+        if self.kernel=='rational_euclidean' or self.kernel=='ratl2':
+            self._kernel = kernels.rational_euclidean_kernel#(xi,xj,self.c)
+            self.kparams = (self.c,)
+            self._dismat = euclidean2
+
+        if self.kernel=='rational_cityblock' or self.kernel=='ratl1':
+            self._kernel = kernels.rational_cityblock_kernel#(xi,xj,self.c)
+            self.kparams = (self.c,)
+            self._dismat = manhattan
+
+        if self.kernel=='spherical_cityblock' or self.kernel=='sphl1':
+            self._kernel = kernels.spherical_cityblock_kernel#(xi,xj,self.gamma)
+            self.kparams = (self.gamma,)
+            self._dismat = manhattan
+
+        if self.kernel=='spherical_euclidean' or self.kernel=='sphl2':
+            self._kernel = kernels.spherical_euclidean_kernel#(xi,xj,self.gamma)
+            self.kparams = (self.gamma,)
+            self._dismat = euclidean2
+
+
+
 
 
     def __repr__(self):
@@ -47,51 +100,20 @@ class krr(object):
 
 
 
-    def _kernel(self,xi,xj):
-        """xxx."""
+    def _fit(self,dmtr,ytr):
+        """Find the regression coefficients and intializes
+        the variable self.coeff.
 
-        if self.kernel=='rbf' or self.kernel=='gaussian':
-            return kernels.rbf_kernel(xi,xj,self.gamma)
+        The fit is splitted in two functions, because self._fit
+        is needed by self.optimize_kernel and self.fit is
+        done to have the same structure as sklearn.
+        """
 
-        if self.kernel=='laplacian':
-            return kernels.laplacian_kernel(xi,xj,self.gamma)
+        ktr=self._kernel(dmtr,*self.kparams)
 
-        if self.kernel=='multiquadric_euclidean' or self.kernel=='mql2':
-            return kernels.multiquadric_euclidean_kernel(xi,xj,self.c)
-
-        if self.kernel=='multiquadric_cityblock' or self.kernel=='mql1':
-            return kernels.multiquadric_cityblock_kernel(xi,xj,self.c)
-
-        if self.kernel=='inv_multiquadric_euclidean' or self.kernel=='imql2':
-            return kernels.inv_multiquadric_euclidean_kernel(xi,xj,self.c)
-
-        if self.kernel=='inv_multiquadric_cityblock' or self.kernel=='imql1':
-            return kernels.inv_multiquadric_cityblock_kernel(xi,xj,self.c)
-
-        if self.kernel=='rational_euclidean' or self.kernel=='ratl2':
-            return kernels.rational_euclidean_kernel(xi,xj,self.c)
-
-        if self.kernel=='rational_cityblock' or self.kernel=='ratl1':
-            return kernels.rational_cityblock_kernel(xi,xj,self.c)
-
-        if self.kernel=='spherical_cityblock' or self.kernel=='sphl1':
-            return kernels.spherical_cityblock_kernel(xi,xj,self.gamma)
-
-        if self.kernel=='spherical_euclidean' or self.kernel=='sphl2':
-            return kernels.spherical_euclidean_kernel(xi,xj,self.gamma)
-
-    def _get_ktr(self,xtr):
-        """Return the kernel of a dataset to itself."""
-
-        return self._kernel(xtr,xtr)
+        self.coeff=np.linalg.solve(ktr+self.alpha*np.eye(ktr.shape[0]),ytr)
 
 
-
-
-    def _get_kte(self,xte,xtr):
-        """Returns the kernel between two datasets."""
-
-        return self._kernel(xte,xtr)
 
 
 
@@ -99,13 +121,35 @@ class krr(object):
     def fit(self,xtr,ytr):
         """Find the regression coefficients and intializes
         the variable self.coeff.
+
+        Version of self._fit for the user.
         """
 
-        self.xtr=xtr
+        self.xtr=xtr # To be used by self.predict without passing it as argument.
 
-        self.ktr=self._get_ktr(self.xtr)
+        dmtr = self._dismat(xtr,xtr)
 
-        self.coeff=np.linalg.solve(self.ktr+self.alpha*np.eye(xtr.shape[0]),ytr)
+        self._fit(dmtr,ytr)
+
+
+
+
+
+
+    def _predict(self,dmte):
+        """Returns the predicted function values for the
+        data points in xte.
+
+        The prediction is splitted in two functions, because self._predict
+        is needed by self.optimize_kernel and self.predict is
+        done to have the same structure as sklearn.
+        """
+
+        kte=self._kernel(dmte,*self.kparams)
+
+        return np.dot(kte,self.coeff)
+
+
 
 
 
@@ -113,26 +157,31 @@ class krr(object):
     def predict(self,xte):
         """Returns the predicted function values for the
         data points in xte.
+
+        Version of self._predict for the user.
         """
 
-        self.kte=self._get_kte(xte,self.xtr)
+        dmte = self._dismat(xte,self.xtr)
 
-        return np.dot(self.kte,self.coeff)
-
-
+        return self._predict(dmte)
 
 
 
-    def _mae(self,gamma,xtr,ytr,xte,yte):
+
+
+
+
+    def _mae(self,gamma,dmtr,dmte,ytr,yte):
         """xxx."""
 
         self.gamma=np.abs(gamma)
 
-        self.fit(xtr,ytr)
+        self._fit(dmtr,ytr)
 
-        yp=self.predict(xte)
+        yp=self._predict(dmte)
 
         return np.abs(yte-yp).sum()/yp.shape[0]
+
 
 
 
@@ -148,11 +197,14 @@ class krr(object):
         Returns the lowest error and updates self.gamma.
         """
 
+        dmtr = self._dismat(xtr,xtr)
+        dmte = self._dismat(xte,xtr)
+
         if optmod=='simplex':
-            self.gamma,err = simplex(    self._mae,gamma0,      xtr, ytr, xte, yte, maxiter, tol)
+            self.gamma,err = simplex(    self._mae, gamma0,      dmtr, dmte, ytr, yte, maxiter, tol)
 
         if optmod=='grid_search':
-            self.gamma,err = grid_search(self._mae,gamma_range, xtr, ytr, xte, yte)
+            self.gamma,err = grid_search(self._mae, gamma_range, dmtr, dmte, ytr, yte)
 
         return err
 
